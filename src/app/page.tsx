4c0596fd89ba72b6f4b1ac30532eb5b32b64cb22
MudasserHaryani <Mudasser.haryani71@gmail.com>
2024-12-09 04:16:42 +0500

    aa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
+
 import Hero from "@/components/Hero";
 import Hero2 from "@/components/Hero2";
 import Hero3 from "@/components/Hero3";
 import Hero4 from "@/components/Hero4";
 import Hero5 from "@/components/Hero5";
 import Hero6 from "@/components/Hero6";
-import Image from "next/image";
 
 export default function Home() {
   return (
